59da3e8e2d9e202700977e63e78f4e5b655e540f
Nick Brennan <nsbrennan@outlook.com>
2025-06-04 00:44:13 -0400

    !!

--- Resources/run.py
+++ Resources/run.py
@@ -1,20 +1,5 @@
-from pybaseball import batting_stats
-
-print("Getting 2023 batting stats...")
-df = batting_stats(2023)
-print(df.head())
-
-from pybaseball import batting_stats
-import pandas as pd
-
-print("Fetching 2023 league-wide batting stats...")
-
-# Get league batting stats
-batting = batting_stats(2023)
-print(batting.head())
-
-# Save to CSV
-batting.to_csv("batting_2023.csv", index=False)
-print("✅ Batting stats saved to batting_2023.csv")
+>>> from pybaseball import standings
+>>> data = standings(2016)[4]
+>>> print(data)
 
 
