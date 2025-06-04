238e06ff5c8149db24b71c5c779ce2edf92b2e35
Nick Brennan <nsbrennan@outlook.com>
2025-06-04 01:02:55 -0400

    !

--- Resources/run.py
+++ Resources/run.py
@@ -1,5 +1,10 @@
->>> from pybaseball import standings
->>> data = standings(2016)[4]
->>> print(data)
+from pybaseball import standings
+data = standings(2016)
+if isinstance(data, list) and len(data) > 4:
+	print(data[4])
+else:
+	print("Unexpected data structure or insufficient elements in the standings.")
+
+
 
 
